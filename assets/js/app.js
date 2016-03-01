7e95441bee578d29a3c3196b222844637013b94c
Larissa Rabello <lari.rabello@gmail.com>
2016-03-01 12:10:11 -0300

    adding .gitignore for node_modules files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://node-workspace-johnnyeric.c9users.io/');
+var socket = io.connect();
 
 socket.on('add', function(data){
     $('#tv_shows').append('<li class="list-group-item"><span data-field="name">'+data.name+'</span><span id="del_'+data.id+'" class="btn-delete badge badge-danger">delete</span><span class="badge badge-info">newly added</span></li>');
@@ -10,17 +10,17 @@ socket.on('del',function(data){
 });
 
 socket.on('user_entered',function(data){
-   console.log(data.user + data.msg); 
+   console.log(data.user + data.msg);
    $('#user-list').append('<li>'+data.user+'</li>');
 });
 
 socket.on('user_list',function(data){
     $('#user-list').html('');
-    
+
     for(var key in data){
         $('#user-list').append('<li><span class="badge badge-success">&nbsp</span> '+data[key]+'</li>');
     }
-   
+
 });
 
 socket.on('recent',function(data){
@@ -50,7 +50,7 @@ $(document).ready(function(){
         socket.emit('add_movie',json); // faster, because there is already a connection, ajax creates http overhead as it creates a connection
         $('#name').val('');
     });
-    
+
      $('#setName').on('click',function(){
         var json = {name : $('#username').val()};
         socket.emit('change_name', json);
@@ -62,5 +62,5 @@ $(document).ready(function(){
         var name =  $(this).closest('li').find('[data-field=name]').html();
         socket.emit('del_movie', {'id': id, 'name' : name});
     });
-    
+
 });
