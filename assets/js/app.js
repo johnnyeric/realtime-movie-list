2bf105105c6765cc4cdef86f07f89920c2ab277c
Johnny Eric <johnnyeric@gmail.com>
2016-03-08 22:04:29 +0000

    Fixed sass import, added browserify and other dependencies but commented

---
+++ assets/js/app.js
@@ -0,0 +1,72 @@
+var socket = io.connect();
+
+socket.on('add', function(data){
+    $('#tv_shows').append('<li class="list-group-item"><span data-field="name">'+data.name+'</span><span id="del_'+data.id+'" class="btn-delete badge badge-danger">delete</span><span class="badge badge-info">newly added</span></li>');
+
+});
+
+socket.on('del',function(data){
+    $('#del_'+data.id).closest('li').remove();
+});
+
+socket.on('user_entered',function(data){
+   console.log(data.user + data.msg);
+   $('#user-list').append('<li>'+data.user+'</li>');
+});
+
+socket.on('user_list',function(data){
+    $('#user-list').html('');
+
+    for(var key in data){
+        $('#user-list').append('<li><span class="badge badge-success">&nbsp</span> '+data[key]+'</li>');
+    }
+
+});
+
+socket.on('recent',function(data){
+    if($('#recent li').length >= 5){
+        $('#recent li:last-child').remove();
+    }
+    var actionMessage = data.action === 'add' ? 'added' : 'removed';
+    $('#recent').prepend('<li>User '+data.user + ' '+actionMessage + ' a movie called ' + data.movie+'</li>');
+});
+
+var $ = require('jquery');
+global.jQuery = global.$ = $;
+
+var bootstrap = require('bootstrap');
+
+
+$(document).ready(function(){
+    $.ajax({
+        type:'GET',
+        url:'tv_shows',
+        dataType: 'json',
+        success: function(data){
+
+            $.each(data,function(idx,el){
+                $('#tv_shows').append('<li class="list-group-item"><span data-field="name">'+el.name+'</span><span id="del_'+el.id+'" class="btn-delete badge badge-danger">delete</span></li>');
+            });
+
+        }
+
+    });
+    $('#add').on('click',function(){
+        var json = {name : $('#name').val()};
+        socket.emit('add_movie',json); // faster, because there is already a connection, ajax creates http overhead as it creates a connection
+        $('#name').val('');
+    });
+
+     $('#setName').on('click',function(){
+        var json = {name : $('#username').val()};
+        socket.emit('change_name', json);
+        $('#username').val('');
+    });
+
+    $('body').on('click','.btn-delete',function(){
+        var id = $(this).attr('id').replace('del_','');
+        var name =  $(this).closest('li').find('[data-field=name]').html();
+        socket.emit('del_movie', {'id': id, 'name' : name});
+    });
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
+'use strict';
+//https://moroccojs.org/tutorials/npm-based-front-end-workflow/
+
+var watchify = require('watchify');
+var browserify = require('browserify');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var gutil = require('gulp-util');
 var path    = require('path');
+var source = require('vinyl-source-stream');
+var assign = require('lodash.assign');
 
 gulp.task('sass', function () {
-  return gulp.src('./assets/css/**/*.sass')
+  return gulp.src('./assets/sass/**/*.sass')
     .pipe(sass({includePaths:[path.join(__dirname, 'node_modules')]}).on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
 });
@@ -11,3 +19,27 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('./assets/sass/**/*.sass', ['sass']);
 });
+
+/* Browserify
+// add custom browserify options here
+var customOpts = {
+  entries: ['./assets/js/app.js'],
+  debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts)); 
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('app.js'))
+    // optional, remove if you don't need to buffer file contents --
+    //.pipe(buffer()) --> var buffer = require('vinyl-buffer');
+    .pipe(gulp.dest('./public/js'));
+}
+*/
